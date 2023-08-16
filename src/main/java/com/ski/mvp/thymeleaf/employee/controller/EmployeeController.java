9334ff9435004820eafc8a5f298be47eeb206337
noisonnoiton <noisonnoiton@gmail.com>
2023-08-16 16:18:39 +0900

    add bff endpoint

--- src/main/java/com/ski/mvp/thymeleaf/employee/controller/EmployeeController.java
+++ src/main/java/com/ski/mvp/thymeleaf/employee/controller/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.ski.mvp.thymeleaf.employee.controller;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,14 +14,27 @@ import com.ski.mvp.thymeleaf.employee.domain.Employee;
 import com.ski.mvp.thymeleaf.employee.domain.EmployeeSearch;
 
 @RestController
-@RequestMapping("/v1")
+@RequestMapping("/v1/employees")
 public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
 
-    @PostMapping("/employees/search")
+    @PostMapping("/search")
     public List<Employee> searchEmployee(@RequestBody EmployeeSearch keyword) throws Exception {
         return this.employeeService.searchEmployee(keyword);
+    }
+
+    @PostMapping("/chart/gender")
+    public List<HashMap<String, Object>> chartEmployeeGender(@RequestBody HashMap<String, Object> keyword) throws Exception {
+        return this.employeeService.chartEmployeeGender(keyword);
+    }
+    @PostMapping("/chart/bloodtype")
+    public List<HashMap<String, Object>> chartEmployeeBloodType(@RequestBody HashMap<String, Object> keyword) throws Exception {
+        return this.employeeService.chartEmployeeBloodType(keyword);
+    }
+    @PostMapping("/chart/combination")
+    public List<HashMap<String, Object>> chartEmployeeCombination(@RequestBody HashMap<String, Object> keyword) throws Exception {
+        return this.employeeService.chartEmployeeCombination(keyword);
     }
 }
--- src/main/java/com/ski/mvp/thymeleaf/employee/service/EmployeeService.java
+++ src/main/java/com/ski/mvp/thymeleaf/employee/service/EmployeeService.java
@@ -2,6 +2,7 @@ package com.ski.mvp.thymeleaf.employee.service;
 
 import java.util.List;
 import java.util.Arrays;
+import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,14 +14,32 @@ import com.ski.mvp.thymeleaf.employee.domain.Employee;
 import com.ski.mvp.thymeleaf.employee.domain.EmployeeSearch;
 
 @Service
+@SuppressWarnings({"unchecked", "rawtypes"})
 public class EmployeeService {
     @Value("${api.springboot.url}")
 	private String apiSpringboot;
 
+    private String apiEmployee = "/api/v1/employees";
+
     @Autowired
-    private RestService<Employee[]> employeeListService;
+    private RestService<Employee[]> employeeSearchService;
+
+    @Autowired
+    private RestService<HashMap[]> employeeChartService;
 
     public List<Employee> searchEmployee(EmployeeSearch keyword) throws Exception {
-		return Arrays.asList(this.employeeListService.post(String.format("%s%s", apiSpringboot, "/api/v1/employees/search"), HttpHeaders.EMPTY, keyword, Employee[].class).getBody());
+		return Arrays.asList(this.employeeSearchService.post(String.format("%s%s%s", apiSpringboot, apiEmployee, "/search"), HttpHeaders.EMPTY, keyword, Employee[].class).getBody());
+	}
+
+    public List<HashMap<String, Object>> chartEmployeeGender(HashMap<String, Object> keyword) throws Exception {
+		return Arrays.asList(this.employeeChartService.post(String.format("%s%s%s", apiSpringboot, apiEmployee, "/chart/gender"), HttpHeaders.EMPTY, keyword, HashMap[].class).getBody());
+	}
+
+    public List<HashMap<String, Object>> chartEmployeeBloodType(HashMap<String, Object> keyword) throws Exception {
+		return Arrays.asList(this.employeeChartService.post(String.format("%s%s%s", apiSpringboot, apiEmployee, "/chart/bloodtype"), HttpHeaders.EMPTY, keyword, HashMap[].class).getBody());
+	}
+
+    public List<HashMap<String, Object>> chartEmployeeCombination(HashMap<String, Object> keyword) throws Exception {
+		return Arrays.asList(this.employeeChartService.post(String.format("%s%s%s", apiSpringboot, apiEmployee, "/chart/combination"), HttpHeaders.EMPTY, keyword, HashMap[].class).getBody());
 	}
 }
